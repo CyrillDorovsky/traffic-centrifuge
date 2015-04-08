514021b030348f776d126127d16fb763e2c3874e
Cyrill Dorovsky <cyrill.dorovsky@gmail.com>
2015-04-08 23:25:36 +0300

    contionoues integration

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 
-get '/' do
-  'Hello World'
+get '/hola' do
+  android_version = request.user_agent[/Android (.*?);/]
+  user_platform =  if android_version
+                     "Android #{ $1 }"
+                   else
+                     "Android"
+                   end
+  erb :hola, locals: { user_platform: user_platform, app_url: 'http://mobile333.com/o/39168' }
 end
