db60a99e38b97fb8797a8b93cd8b7d666a92ab79
Cyrill Dorovsky <cyrill.dorovsky@gmail.com>
2015-05-13 12:35:08 +0300

    finish lendings

--- app.rb
+++ app.rb
@@ -5,8 +5,9 @@ $LOAD_PATH << '.'
 require 'message'
 require 'advert'
 
+apps_list = Advert.index
 get '/' do
-  haml :index, locals: { messages: Message.index, apps_list: Advert.apps }
+  haml :index, locals: { messages: Message.index, apps_list: Advert.index }
 end
 
 get '/apps' do
@@ -14,7 +15,17 @@ get '/apps' do
 end
 
 get '/games' do
-  haml :index, locals: { messages: Message.index, apps_list: Advert.apps }
+  haml :index, locals: { messages: Message.index, apps_list: Advert.games }
+end
+
+get '/apps/:name' do
+  app_name = params[ :name ]
+  haml :landing, locals: { app: apps_list[ app_name.to_sym ], app_name: app_name }
+end
+
+get '/games/:name' do
+  app_name = params[ :name ]
+  haml :landing, locals: { app: apps_list[ app_name.to_sym ], app_name: app_name }
 end
 
 
