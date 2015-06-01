de4bba794dd89cd11bd1ba537c0f4da11df58fb7
Cyrill Dorovsky <cyrill.dorovsky@gmail.com>
2015-06-01 14:49:35 +0300

    finally checking access to another redis

--- app.rb
+++ app.rb
@@ -1,17 +1,36 @@
 require 'sinatra'
-require 'slim'
 require 'hashids'
+require 'json'
+require 'redis'
 
 configure do
+
+  if ENV['REDISCLOUD_URL']
+    uri = URI.parse(ENV["REDISCLOUD_URL"])
+    $redis = Redis.new( :host => uri.host, :port => uri.port, :password => uri.password )
+  else
+    $redis = Redis.new host: 'localhost', port: 6379
+  end
+
   enable :sessions
-  hashids = Hashids.new( 'somesalt' ) #ENV[ 'HASHID_SALT' ] )
 end
 
-#helpers do
-#end
+helpers do
+  def hashid
+    Hashids.new( 'richpays number one' ) #ENV[ 'HASHID_SALT' ] )
+  end
+end
 
-get '/:redirect_code.?:format' do
-  offer = hashids.decode( params[ :redirect_code ] )
-  #offer = $redis.get( "direct_offer_#{ offer_id }" )
-  json offer: offer
+get '/' do
+  root_opts = { redorect_code: hashid.encode( ) }
+  headers \
+    "Content-Type" => "application/json"
+  body JSON.generate( root_opts )
+end
+
+get '/:redirect_code' do
+  offer = $redis.get( "direct_offer_#{ params[ :redirect_code ] }" )
+  headers \
+    "Content-Type" => "application/json"
+  body offer
 end
