c89327d8e8cd0189ad1acab7e515611b14a88cfe
Cyrill Dorovsky <cyrill.dorovsky@gmail.com>
2015-07-01 02:23:45 +0300

    emulator regular events

--- amqp_regular.rb
+++ amqp_regular.rb
@@ -1,10 +1,10 @@
 require 'bunny'
 class AmqpRegular
-  def initialize( message )
+  def initialize( queue_name, message )
     bunny = Bunny.new ENV['CLOUDAMQP_URL']
     bunny.start
     bunny_channel  = bunny.create_channel
-    regular_tasks = bunny_channel.queue( "conversion_sender", durable: true )
+    regular_tasks = bunny_channel.queue( queue_name, durable: true )
     regular_tasks.publish message
   end
 end
--- clock.rb
+++ clock.rb
@@ -3,4 +3,12 @@ require './amqp_regular'
 
 include Clockwork
 
-every( 1.minute, 'random conversion' ) { AmqpRegular.new( 'random_conversion' ) }
+if ENV['EMULATOR'] == 'ON'
+every( 1.second, 'offer_set_show' ) { AmqpRegular.new( 'emulate_visitors', 'campaign_visitor' ) }
+every( 1.second, 'offer_set_show' ) { AmqpRegular.new( 'emulate_visitors', 'offer_set_visitor' ) }
+every( 1.second, 'offer_set_show' ) { AmqpRegular.new( 'emulate_visitors', 'direct_offer_visitor' ) }
+
+every( 10.seconds, 'random conversion' ) { AmqpRegular.new( 'conversion_sender', 'mongo_offer_set_show' ) }
+every( 10.seconds, 'random conversion' ) { AmqpRegular.new( 'conversion_sender', 'pg_offer_set_show' ) }
+every( 10.seconds, 'random conversion' ) { AmqpRegular.new( 'conversion_sender', 'pg_direct_offer' ) }
+end
