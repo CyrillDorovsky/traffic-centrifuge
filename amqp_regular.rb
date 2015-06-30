32af5b6a648d088bceba86c0d917dd117a2ba84d
Cyrill Dorovsky <cyrill.dorovsky@gmail.com>
2015-06-30 21:23:23 +0300

    conversion sender

--- amqp_regular.rb
+++ amqp_regular.rb
@@ -4,7 +4,7 @@ class AmqpRegular
     bunny = Bunny.new ENV['CLOUDAMQP_URL']
     bunny.start
     bunny_channel  = bunny.create_channel
-    regular_tasks = bunny_channel.queue( "regular_tasks", durable: true )
+    regular_tasks = bunny_channel.queue( "conversion_sender", durable: true )
     regular_tasks.publish message
   end
 end
