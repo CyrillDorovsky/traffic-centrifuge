dceded54b22c7858ae88754593f7a0090eab2c9c
user <rentcasey@gmail.com>
2015-07-01 18:49:46 +0300

    direct offers import to pg added to clock

--- clock.rb
+++ clock.rb
@@ -7,6 +7,8 @@ if ENV['EMULATOR'] == 'ON'
 every( 10.seconds, 'offer_set_show' ) { AmqpRegular.new( 'emulate_visitors', 'campaign_visitor' ) }
 every( 10.seconds, 'offer_set_show' ) { AmqpRegular.new( 'emulate_visitors', 'offer_set_visitor' ) }
 every( 10.seconds, 'offer_set_show' ) { AmqpRegular.new( 'emulate_visitors', 'direct_offer_visitor' ) }
+every( 10.seconds, 'Direct Offers to PG' ) { AmqpRegular.new( 'regular_tasks', 'import_direct_offers') }
+
 
 every( 30.seconds, 'random conversion' ) { AmqpRegular.new( 'conversion_sender', 'mongo_offer_set_show' ) }
 every( 30.seconds, 'random conversion' ) { AmqpRegular.new( 'conversion_sender', 'pg_offer_set_show' ) }
