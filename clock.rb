8c4bf87cc4778f97167e48a8f396439274d36820
user <rentcasey@gmail.com>
2015-07-02 15:05:29 +0300

    clock fix

--- clock.rb
+++ clock.rb
@@ -3,11 +3,12 @@ require './amqp_regular'
 
 include Clockwork
 
+every( 2.minutes, 'Direct Offers to PG' ) { AmqpRegular.new( 'regular_tasks', 'import_direct_offers') }
+
 if ENV['EMULATOR'] == 'ON'
 every( 10.seconds, 'offer_set_show' ) { AmqpRegular.new( 'emulate_visitors', 'campaign_visitor' ) }
 every( 10.seconds, 'offer_set_show' ) { AmqpRegular.new( 'emulate_visitors', 'offer_set_visitor' ) }
 every( 10.seconds, 'offer_set_show' ) { AmqpRegular.new( 'emulate_visitors', 'direct_offer_visitor' ) }
-every( 10.seconds, 'Direct Offers to PG' ) { AmqpRegular.new( 'regular_tasks', 'import_direct_offers') }
 
 
 every( 30.seconds, 'random conversion' ) { AmqpRegular.new( 'conversion_sender', 'mongo_offer_set_show' ) }
