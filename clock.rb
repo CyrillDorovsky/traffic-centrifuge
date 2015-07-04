48a6dd567201849d72016ed118fc71ef0d715be5
user <rentcasey@gmail.com>
2015-07-04 15:00:36 +0300

    clock slow down

--- clock.rb
+++ clock.rb
@@ -6,12 +6,12 @@ include Clockwork
 every( 2.minutes, 'Direct Offers to PG' ) { AmqpRegular.new( 'regular_tasks', 'import_direct_offers') }
 
 if ENV['EMULATOR'] == 'ON'
-every( 10.seconds, 'offer_set_show' ) { AmqpRegular.new( 'emulate_visitors', 'campaign_visitor' ) }
-every( 10.seconds, 'offer_set_show' ) { AmqpRegular.new( 'emulate_visitors', 'offer_set_visitor' ) }
-every( 10.seconds, 'offer_set_show' ) { AmqpRegular.new( 'emulate_visitors', 'direct_offer_visitor' ) }
+every( 2.minutes, 'offer_set_show' ) { AmqpRegular.new( 'emulate_visitors', 'campaign_visitor' ) }
+every( 2.minutes, 'offer_set_show' ) { AmqpRegular.new( 'emulate_visitors', 'offer_set_visitor' ) }
+every( 2.minutes, 'offer_set_show' ) { AmqpRegular.new( 'emulate_visitors', 'direct_offer_visitor' ) }
 
 
-every( 30.seconds, 'random conversion' ) { AmqpRegular.new( 'conversion_sender', 'mongo_offer_set_show' ) }
-every( 30.seconds, 'random conversion' ) { AmqpRegular.new( 'conversion_sender', 'pg_offer_set_show' ) }
-every( 30.seconds, 'random conversion' ) { AmqpRegular.new( 'conversion_sender', 'pg_direct_offer' ) }
+every( 5.minutes, 'random conversion' ) { AmqpRegular.new( 'conversion_sender', 'mongo_offer_set_show' ) }
+every( 10.minutes, 'random conversion' ) { AmqpRegular.new( 'conversion_sender', 'pg_offer_set_show' ) }
+every( 10.minutes, 'random conversion' ) { AmqpRegular.new( 'conversion_sender', 'pg_direct_offer' ) }
 end
